2cb940eb2381b4f11d41844719b289cf4594bb79
bartosz.maslanka external <bartosz.maslanka.external@atos.net>
2021-11-21 23:49:56 +0100

    Dash app works

--- app/app.py
+++ app/app.py
@@ -1,52 +1,32 @@
 import dash
 from dash.dependencies import Input, Output
-import cv2
-from layout.layout import layout, gen_sample_fig
+
+from layout.layout import layout, get_figure, get_images
 import plotly.express as px
 import pandas as pd
 import numpy as np
-lastPoint = None
-temp = None
+
+last_point = 0
+temp = 1
 app = dash.Dash(__name__)
 app.title = 'Cell app'
 app.layout = layout
-data = pd.read_csv(r'../data/features.csv')[['name']].values.tolist()
+
 IMAGE_DIR_PATH = r'../resized_merged_images'
+
+
 @app.callback(
-    Output('image', 'figure'),
-    Input('graph', 'clickData')
+    Output('image', 'figure')
+    ,
+    [Input('graph', 'clickData')]
 )
-def update_text(choosenPoint):
-    global lastPoint, temp
-    print('here')
-    if choosenPoint:
-        print(choosenPoint['points'][0]['pointIndex'])
-        if temp is None:
-            temp = choosenPoint
-            ids = choosenPoint['points'][0]['pointIndex']
-            path = data[ids]
+def update_text(choosen_point):
+    img1, img2, file_name_1, file_name_2 = get_images(choosen_point)
+    fig = get_figure(img2, img1, file_name_2, file_name_1)
+    return fig
 
-            img = cv2.imread(IMAGE_DIR_PATH+'/'+path[0], cv2.IMREAD_GRAYSCALE)
-            fig = px.imshow(img, binary_string=True)
-            return fig
-        else:
-            lastPoint = temp
-            temp = choosenPoint
-            ids1 = lastPoint['points'][0]['pointIndex']
-            path1 = data[ids1]
-            img1 = cv2.imread(IMAGE_DIR_PATH+'/'+path1[0], cv2.IMREAD_GRAYSCALE)
 
-            ids2 = choosenPoint['points'][0]['pointIndex']
-            path2 = data[ids2]
-            img2 = cv2.imread(IMAGE_DIR_PATH+'/'+path2[0], cv2.IMREAD_GRAYSCALE)
-
-            fig = px.imshow(np.array([img2, img1]), facet_col=0, binary_string=True, facet_col_wrap=1,
-                            facet_row_spacing=0.35, width=700, height=1000)
-            return fig
-
-    #             return (str(lastPoint['points'][0]['pointIndex'])+'\n'
-    #             + str(choosenPoint['points'][0]['pointIndex']))
-    return gen_sample_fig()
+# return gen_sample_fig()
 
 
 if __name__ == '__main__':
--- app/layout/layout.py
+++ app/layout/layout.py
@@ -1,47 +1,105 @@
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import dcc, html
 import plotly.express as px
 import numpy as np
 import pandas as pd
-import os
+import plotly.graph_objects as go
+import cv2
 
-def gen_umap_fig(data: np.array):
-    fig = px.scatter(x=data[:, 0], y=data[:, 1])
+IMAGE_DIR_PATH = r'../resized_merged_images'
+last_point = None
+temp = {'points': [{'pointIndex': 0}]}
+
+
+def gen_umap_fig(data: pd.DataFrame):
+    fig = go.Figure(data=go.Scatter(x=data['vector1'],
+                                    y=data['vector2'],
+                                    mode='markers',
+                                    text=data['name']))
+    fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+    fig.update_layout(
+        plot_bgcolor='#f2f2f2',
+        font_family='Courier New',
+        paper_bgcolor='#f2f2f2',
+        xaxis=dict(showgrid=False,
+                   showline=False,
+                   zeroline=False),
+        yaxis=dict(showgrid=False,
+                   showline=False,
+                   zeroline=False),
+        margin=dict(l=0, r=0, b=0, t=0),
+    )
+    fig.update_traces(marker=dict(size=5,
+                                  color='#ffd359'))
     return fig
 
 
-def gen_sample_fig():
-    fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
+def get_images(choosen_point=None):
+    if choosen_point is None:
+        file_name_1 = image_names[0][0]
+        img1 = cv2.imread(IMAGE_DIR_PATH + '/' + file_name_1, cv2.IMREAD_GRAYSCALE)
+        file_name_2 = image_names[1][0]
+        img2 = cv2.imread(IMAGE_DIR_PATH + '/' + file_name_2, cv2.IMREAD_GRAYSCALE)
+        return img1, img2, file_name_1, file_name_2
+    global last_point, temp
+    last_point = temp
+    temp = choosen_point
+    ids1 = last_point['points'][0]['pointIndex']
+    file_name_1 = image_names[ids1][0]
+    img1 = cv2.imread(IMAGE_DIR_PATH + '/' + file_name_1, cv2.IMREAD_GRAYSCALE)
+
+    ids2 = choosen_point['points'][0]['pointIndex']
+    file_name_2 = image_names[ids2][0]
+    img2 = cv2.imread(IMAGE_DIR_PATH + '/' + file_name_2, cv2.IMREAD_GRAYSCALE)
+    return img1, img2, file_name_1, file_name_2
+
+
+def get_figure(image_1, image_2, name_1, name_2):
+    fig = px.imshow(np.array([image_1, image_2]), facet_col=0, binary_string=True,
+                    facet_col_spacing=0.02, width=800, height=800, labels={'facet_col': 'Image'},
+                    )
+    fig.update_xaxes(showticklabels=False).update_yaxes(showticklabels=False)
+    fig.update_traces(hovertemplate=None, hoverinfo='skip')
+    fig.layout.annotations[0]['text'] = f'Image: {name_1}'
+    fig.layout.annotations[1]['text'] = f'Image: {name_2}'
+    fig.update_layout(
+        plot_bgcolor='#f2f2f2',
+        font_family='Courier New',
+        paper_bgcolor='#f2f2f2',
+        margin=dict(l=0, r=0, b=0, t=20))
     return fig
 
-print(os.getcwd())
-data = pd.read_csv(r'../data/features.csv')[['vector1', 'vector2']].to_numpy()
+
+data = pd.read_csv(r'../data/features.csv')
+image_names = data[['name']].values.tolist()
+img1, img2, file_name_1, file_name_2 = get_images()
 layout = html.Div([
-    html.Div([  # header
+    html.Div([
 
-    ], className='header', id='app-header', style={'height': '100px'}),
-    html.Div([  # navbar
+    ], className='header', id='app-header', style={'background': 'gray'}),
+    html.Div([
 
-    ], className='navbar', id='navbar', style={'height': '100px'}),
-    html.Div([  # mainpart
-        #         html.Div([
-        # #             dcc.Dropdown()
-        #         ], className='left_side', style={'height': '600px', 'float':'left', 'width':'20%'}),
+    ], className='navbar', id='navbar', style={}),
+    html.Div([
         html.Div([
             dcc.Graph(id='graph', figure=gen_umap_fig(data))
-        ], className='middle_side', style={'height': '600px', 'float': 'left', 'width': '60%'}),
-        html.Div([
-            #             dcc.Textarea(
-            #             id='textarea',
-            #             value='nothing',
-            #             )
-            dcc.Graph(id='image', figure=gen_sample_fig())
+        ], className='middle_side', style={'height': '500px',
+                                           'float': 'left',
+                                           'width': '50%'}),
 
-        ], className='rightside', style={'height': '600px', 'float': 'left', 'width': '40%', 'padding': '-50px'})
+    ], className='main_part', style={}),
+    html.Div([
 
-    ], className='main_part', style={'background-color': 'green'}),
-    html.Div([  # footer
+        dcc.Graph(id='image', figure=get_figure(img1, img2, file_name_1, file_name_2))
+
+    ], className='rightside', style={'float': 'left',
+                                     'width': '50%',
+                                     'height': '1000'}),
+    html.Div([
 
     ], className='footer')
 
-], className='body')
+], className='body', style={
+    'background-color': '#ebebeb',
+    'margin-left': '60px',
+    'margin-top': '60px'
+})
